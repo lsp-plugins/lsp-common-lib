1286fc47cccbaaf787ee8019d937d15f6fba2465
SadKo <lsp.plugin@gmail.com>
2020-04-01 00:21:00 +0300

    Added endianess functions

---
+++ include/lsp-plug.in/common/arch/x86/endian.h
@@ -0,0 +1,329 @@
+/*
+ * endian.h
+ *
+ *  Created on: 1 апр. 2020 г.
+ *      Author: sadko
+ */
+
+#ifndef LSP_PLUG_IN_COMMON_ARCH_X86_ENDIAN_H_
+#define LSP_PLUG_IN_COMMON_ARCH_X86_ENDIAN_H_
+
+namespace lsp
+{
+    inline uint16_t __lsp_forced_inline    byte_swap(uint16_t v)
+    {
+        ARCH_X86_ASM (
+            __ASM_EMIT("ror $8, %[v]")
+            : [v] "+r"(v)
+            : : "cc"
+        );
+        return v;
+    }
+
+    inline uint32_t __lsp_forced_inline    byte_swap(uint32_t v)
+    {
+        ARCH_X86_ASM (
+            __ASM_EMIT("bswap %[v]")
+            : [v] "+r"(v)
+            : :
+        );
+        return v;
+    }
+
+    inline float __lsp_forced_inline    byte_swap(float v)
+    {
+        ARCH_X86_ASM (
+            __ASM_EMIT("bswap %[v]")
+            : [v] "+r"(v)
+            : :
+        );
+        return v;
+    }
+
+    #ifdef ARCH_X86_64
+        inline uint64_t __lsp_forced_inline    byte_swap(uint64_t v)
+        {
+            ARCH_X86_ASM (
+                __ASM_EMIT("bswap %[v]")
+                : [v] "+r"(v)
+                : :
+            );
+            return v;
+        }
+
+        inline double __lsp_forced_inline    byte_swap(double v)
+        {
+            ARCH_X86_ASM (
+                __ASM_EMIT("bswap %[v]")
+                : [v] "+r"(v)
+                : :
+            );
+            return v;
+        }
+
+    #else /* ARCH_I386 */
+        inline uint64_t __lsp_forced_inline    byte_swap(uint64_t v)
+        {
+            ARCH_X86_ASM (
+                __ASM_EMIT("bswap %%eax")
+                __ASM_EMIT("bswap %%edx")
+                __ASM_EMIT("xchg %%edx, %%eax")
+                : "+A"(v)
+                : :
+            );
+            return v;
+        }
+
+        inline double __lsp_forced_inline    byte_swap(double v)
+        {
+            ARCH_X86_ASM (
+                __ASM_EMIT("bswap %%eax")
+                __ASM_EMIT("bswap %%edx")
+                __ASM_EMIT("xchg %%edx, %%eax")
+                : "+A"(v)
+                : :
+            );
+            return v;
+        }
+
+    #endif  /* ARCH_X86_64 */
+
+    inline void __lsp_forced_inline    byte_swap(uint16_t *v, size_t n)
+    {
+        uint16_t tmp;
+        ARCH_X86_ASM (
+            __ASM_EMIT("test    %[n], %[n]")
+            __ASM_EMIT("jz      2f")
+            __ASM_EMIT("1:")
+            __ASM_EMIT("lodsw")
+            __ASM_EMIT("ror     $8, %[tmp]")
+            __ASM_EMIT("dec     %[n]")
+            __ASM_EMIT("mov     %[tmp], -2(%[v])")
+            __ASM_EMIT("jnz     1b")
+            __ASM_EMIT("2:")
+            : [v] "+S"(v), [n] "+r" (n), [tmp] "=&a"(tmp)
+            : : "cc", "memory"
+        );
+    }
+
+    inline void __lsp_forced_inline    byte_swap(int16_t *v, size_t n)
+    {
+        uint16_t tmp;
+        ARCH_X86_ASM (
+            __ASM_EMIT("test    %[n], %[n]")
+            __ASM_EMIT("jz      2f")
+            __ASM_EMIT("1:")
+            __ASM_EMIT("lodsw")
+            __ASM_EMIT("ror     $8, %[tmp]")
+            __ASM_EMIT("dec     %[n]")
+            __ASM_EMIT("mov     %[tmp], -2(%[v])")
+            __ASM_EMIT("jnz     1b")
+            __ASM_EMIT("2:")
+            : [v] "+S"(v), [n] "+r" (n), [tmp] "=&a"(tmp)
+            : : "cc", "memory"
+        );
+    }
+
+    inline void __lsp_forced_inline    byte_swap(uint32_t *v, size_t n)
+    {
+        uint32_t tmp;
+        ARCH_X86_ASM (
+            __ASM_EMIT("test    %[n], %[n]")
+            __ASM_EMIT("jz      2f")
+            __ASM_EMIT("1:")
+            __ASM_EMIT("lodsl")
+            __ASM_EMIT("bswap   %[tmp]")
+            __ASM_EMIT("dec     %[n]")
+            __ASM_EMIT("mov     %[tmp], -4(%[v])")
+            __ASM_EMIT("jnz     1b")
+            __ASM_EMIT("2:")
+            : [v] "+S"(v), [n] "+r" (n), [tmp] "=&a"(tmp)
+            : : "cc", "memory"
+        );
+    }
+
+    inline void __lsp_forced_inline    byte_swap(int32_t *v, size_t n)
+    {
+        uint32_t tmp;
+        ARCH_X86_ASM (
+            __ASM_EMIT("test    %[n], %[n]")
+            __ASM_EMIT("jz      2f")
+            __ASM_EMIT("1:")
+            __ASM_EMIT("lodsl")
+            __ASM_EMIT("bswap   %[tmp]")
+            __ASM_EMIT("dec     %[n]")
+            __ASM_EMIT("mov     %[tmp], -4(%[v])")
+            __ASM_EMIT("jnz     1b")
+            __ASM_EMIT("2:")
+            : [v] "+S"(v), [n] "+r" (n), [tmp] "=&a"(tmp)
+            : : "cc", "memory"
+        );
+    }
+
+    inline void __lsp_forced_inline    byte_swap(float *v, size_t n)
+    {
+        uint32_t tmp;
+        ARCH_X86_ASM (
+            __ASM_EMIT("test    %[n], %[n]")
+            __ASM_EMIT("jz      2f")
+            __ASM_EMIT("1:")
+            __ASM_EMIT("lodsl")
+            __ASM_EMIT("bswap   %[tmp]")
+            __ASM_EMIT("dec     %[n]")
+            __ASM_EMIT("mov     %[tmp], -4(%[v])")
+            __ASM_EMIT("jnz     1b")
+            __ASM_EMIT("2:")
+            : [v] "+S"(v), [n] "+r" (n), [tmp] "=&a"(tmp)
+            : : "cc", "memory"
+        );
+    }
+
+    #ifdef ARCH_X86_64
+        inline void __lsp_forced_inline    byte_swap(uint64_t *v, size_t n)
+        {
+            uint64_t tmp;
+            ARCH_X86_ASM (
+                __ASM_EMIT("test    %[n], %[n]")
+                __ASM_EMIT("jz      2f")
+                __ASM_EMIT("1:")
+                __ASM_EMIT("lodsq")
+                __ASM_EMIT("bswap   %[tmp]")
+                __ASM_EMIT("dec     %[n]")
+                __ASM_EMIT("mov     %[tmp], -8(%[v])")
+                __ASM_EMIT("jnz     1b")
+                __ASM_EMIT("2:")
+                : [v] "+S"(v), [n] "+r" (n), [tmp] "=&a"(tmp)
+                : : "cc", "memory"
+            );
+        }
+
+        inline void __lsp_forced_inline    byte_swap(int64_t *v, size_t n)
+        {
+            uint64_t tmp;
+            ARCH_X86_ASM (
+                __ASM_EMIT("test    %[n], %[n]")
+                __ASM_EMIT("jz      2f")
+                __ASM_EMIT("1:")
+                __ASM_EMIT("lodsq")
+                __ASM_EMIT("bswap   %[tmp]")
+                __ASM_EMIT("dec     %[n]")
+                __ASM_EMIT("mov     %[tmp], -8(%[v])")
+                __ASM_EMIT("jnz     1b")
+                __ASM_EMIT("2:")
+                : [v] "+S"(v), [n] "+r" (n), [tmp] "=&a"(tmp)
+                : : "cc", "memory"
+            );
+        }
+
+        inline void __lsp_forced_inline    byte_swap(double *v, size_t n)
+        {
+            uint64_t tmp;
+            ARCH_X86_ASM (
+                __ASM_EMIT("test    %[n], %[n]")
+                __ASM_EMIT("jz      2f")
+                __ASM_EMIT("1:")
+                __ASM_EMIT("lodsq")
+                __ASM_EMIT("bswap   %[tmp]")
+                __ASM_EMIT("dec     %[n]")
+                __ASM_EMIT("mov     %[tmp], -8(%[v])")
+                __ASM_EMIT("jnz     1b")
+                __ASM_EMIT("2:")
+                : [v] "+S"(v), [n] "+r" (n), [tmp] "=&a"(tmp)
+                : : "cc", "memory"
+            );
+        }
+
+    #else /* ARCH_I386 */
+
+        inline void __lsp_forced_inline    byte_swap(uint64_t *v, size_t n)
+        {
+            uint32_t a, b;
+            ARCH_X86_ASM (
+                __ASM_EMIT("test    %[n], %[n]")
+                __ASM_EMIT("jz      2f")
+                __ASM_EMIT("1:")
+                __ASM_EMIT("mov     (%[v]), %[a]")
+                __ASM_EMIT("mov     4(%[v]), %[b]")
+                __ASM_EMIT("bswap   %[a]")
+                __ASM_EMIT("bswap   %[b]")
+                __ASM_EMIT("mov     %[b], (%[v])")
+                __ASM_EMIT("mov     %[a], 4(%[v])")
+                __ASM_EMIT("add     $8, %[v]")
+                __ASM_EMIT("dec     %[n]")
+                __ASM_EMIT("jnz     1b")
+                __ASM_EMIT("2:")
+                : [v] "+r"(v), [n] "+r" (n), [a] "=&r" (a), [b] "=&r" (b)
+                : : "cc", "memory"
+            );
+        }
+
+        inline void __lsp_forced_inline    byte_swap(int64_t *v, size_t n)
+        {
+            uint32_t a, b;
+            ARCH_X86_ASM (
+                __ASM_EMIT("test    %[n], %[n]")
+                __ASM_EMIT("jz      2f")
+                __ASM_EMIT("1:")
+                __ASM_EMIT("mov     (%[v]), %[a]")
+                __ASM_EMIT("mov     4(%[v]), %[b]")
+                __ASM_EMIT("bswap   %[a]")
+                __ASM_EMIT("bswap   %[b]")
+                __ASM_EMIT("mov     %[b], (%[v])")
+                __ASM_EMIT("mov     %[a], 4(%[v])")
+                __ASM_EMIT("add     $8, %[v]")
+                __ASM_EMIT("dec     %[n]")
+                __ASM_EMIT("jnz     1b")
+                __ASM_EMIT("2:")
+                : [v] "+r"(v), [n] "+r" (n), [a] "=&r" (a), [b] "=&r" (b)
+                : : "cc", "memory"
+            );
+        }
+
+        inline void __lsp_forced_inline    byte_swap(double *v, size_t n)
+        {
+            uint32_t a, b;
+            ARCH_X86_ASM (
+                __ASM_EMIT("test    %[n], %[n]")
+                __ASM_EMIT("jz      2f")
+                __ASM_EMIT("1:")
+                __ASM_EMIT("mov     (%[v]), %[a]")
+                __ASM_EMIT("mov     4(%[v]), %[b]")
+                __ASM_EMIT("bswap   %[a]")
+                __ASM_EMIT("bswap   %[b]")
+                __ASM_EMIT("mov     %[b], (%[v])")
+                __ASM_EMIT("mov     %[a], 4(%[v])")
+                __ASM_EMIT("add     $8, %[v]")
+                __ASM_EMIT("dec     %[n]")
+                __ASM_EMIT("jnz     1b")
+                __ASM_EMIT("2:")
+                : [v] "+r"(v), [n] "+r" (n), [a] "=&r"(a), [b] "=&r"(b)
+                : : "cc", "memory"
+            );
+        }
+    #endif /* ARCH_X86_64 */
+
+    #if defined(WCHART_16BIT)
+        inline wchar_t __lsp_forced_inline    byte_swap(wchar_t v)
+        {
+            ARCH_X86_ASM (
+                __ASM_EMIT("ror $8, %[v]")
+                : [v] "+r"(v)
+                : : "cc"
+            );
+            return v;
+        }
+    #elif defined(WCHART_32BIT)
+        inline wchar_t __lsp_forced_inline    byte_swap(wchar_t v)
+        {
+            ARCH_X86_ASM (
+                __ASM_EMIT("bswap %[v]")
+                : [v] "+r"(v)
+                : :
+            );
+            return v;
+        }
+    #endif /* PLATFORM_WINDOWS */
+
+}
+
+#endif /* LSP_PLUG_IN_COMMON_ARCH_X86_ENDIAN_H_ */
--- src/Makefile.d
+++ src/Makefile.d
@@ -1,4 +1,29 @@
 $(LSP_COMMON_LIB_BIN)/main/status.o: main/status.cpp \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/status.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/types.h
-$(LSP_COMMON_LIB_BIN)/test/main.o: test/main.cpp
+$(LSP_COMMON_LIB_BIN)/test/utest/endian.o: test/utest/endian.cpp \
+ $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/endian.h \
+ $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/types.h \
+ $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/arch/x86/endian.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/utest.h \
+ $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/status.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/test.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/env.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/main/config.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/types.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/main/dynarray.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/ByteBuffer.h
+$(LSP_COMMON_LIB_BIN)/test/utest/atomic.o: test/utest/atomic.cpp \
+ $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/atomic.h \
+ $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/types.h \
+ $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/arch/x86/atomic.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/utest.h \
+ $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/status.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/test.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/env.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/main/config.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/types.h \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/main/dynarray.h
+$(LSP_COMMON_LIB_BIN)/test/main.o: test/main.cpp \
+ $(LSP_TEST_FW_INC)/lsp-plug.in/test-fw/main.h \
+ $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/types.h
