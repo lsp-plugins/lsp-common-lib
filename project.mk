0436f7b3b1996f39e8aa805ab03ba3e36f16cd86
SadKo <lsp.plugin@gmail.com>
2020-04-03 20:29:27 +0300

    Updated build

--- project.mk
+++ project.mk
@@ -1,6 +1,7 @@
 # Package version
 ARTIFACT_NAME               = lsp-common-lib
 ARTIFACT_VARS               = LSP_COMMON_LIB
+ARTIFACT_HEADERS            = lsp-plug.in
 VERSION                     = 1.0.1-devel
 
 # List of dependencies
