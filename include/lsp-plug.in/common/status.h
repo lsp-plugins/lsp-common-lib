21d3f6042f8c0c179a451bfadffd793baf0184af
SadKo <lsp.plugin@gmail.com>
2020-04-09 00:07:19 +0300

    Added assertions

--- include/lsp-plug.in/common/status.h
+++ include/lsp-plug.in/common/status.h
@@ -10,6 +10,10 @@
 
 #include <lsp-plug.in/common/types.h>
 
+#define LSP_STATUS_ASSERT(x, ...)       { status_t __res__ = (x); if (__res__ != STATUS_OK) { __VA_ARGS__; return __res__; } }
+#define LSP_VSTATUS_ASSERT(x, ...)      { status_t __res__ = (x); if (__res__ != STATUS_OK) { __VA_ARGS__; return; } }
+#define LSP_BOOL_ASSERT(x, res, ...)    { if (!(x)) { __VA_ARGS__; return res; } }
+
 namespace lsp
 {
     /**
