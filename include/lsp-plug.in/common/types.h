87c0a5804feb0d258b22887fee62eb4068c77fe7
SadKo <lsp.plugin@gmail.com>
2020-05-02 20:02:29 +0300

    Updated version

--- include/lsp-plug.in/common/types.h
+++ include/lsp-plug.in/common/types.h
@@ -17,7 +17,7 @@
 // Version of headers
 #define LSP_COMMON_LIB_MAJOR            1
 #define LSP_COMMON_LIB_MINOR            0
-#define LSP_COMMON_LIB_MICRO            2
+#define LSP_COMMON_LIB_MICRO            3
 
 //-----------------------------------------------------------------------------
 // Detect build architecture
--- project.mk
+++ project.mk
@@ -2,7 +2,7 @@
 ARTIFACT_NAME               = lsp-common-lib
 ARTIFACT_VARS               = LSP_COMMON_LIB
 ARTIFACT_HEADERS            = lsp-plug.in
-VERSION                     = 1.0.2
+VERSION                     = 1.0.3-devel
 
 # List of dependencies
 TEST_DEPENDENCIES := \
