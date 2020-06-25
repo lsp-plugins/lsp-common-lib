08d081132785bd024abfaf8a83d833617d4e071c
sadko4u <sadko4u@gmail.com>
2020-06-26 00:37:48 +0300

    Pre-release

--- project.mk
+++ project.mk
@@ -2,7 +2,7 @@
 ARTIFACT_NAME               = lsp-common-lib
 ARTIFACT_VARS               = LSP_COMMON_LIB
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.6-devel
+ARTIFACT_VERSION            = 1.0.6
 
 # List of dependencies
 TEST_DEPENDENCIES := \
