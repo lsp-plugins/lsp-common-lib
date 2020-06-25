1df0e675ffa17d0e90fe47ae6904df28d40cc0e7
sadko4u <sadko4u@gmail.com>
2020-06-26 00:48:26 +0300

    Updated dependencies

--- dependencies.mk
+++ dependencies.mk
@@ -1,5 +1,5 @@
 # Variables that describe dependencies
-LSP_TEST_FW_VERSION        := 1.0.3
+LSP_TEST_FW_VERSION        := 1.0.4
 LSP_TEST_FW_NAME           := lsp-test-fw
 LSP_TEST_FW_TYPE           := src
 LSP_TEST_FW_URL            := https://github.com/sadko4u/$(LSP_TEST_FW_NAME).git
