ea3a24114b6d9097d9a50e020d8c85c1af4d166d
sadko4u <sadko4u@gmail.com>
2021-12-01 02:42:56 +0300

    Added support of RISC-V definitions

--- include/lsp-plug.in/common/types.h
+++ include/lsp-plug.in/common/types.h
@@ -53,8 +53,10 @@
 //-----------------------------------------------------------------------------
 // Detect build architecture
 #if defined(__x86_64__) || defined(__x86_64) || defined(__amd64__) || defined(__amd64) || defined(_M_AMD64)
+    #define ARCH_X86
     #define ARCH_X86_64
 #elif defined(__i386__) || defined(__i386)
+    #define ARCH_X86
     #define ARCH_I386
 #elif defined(__aarch64__)
     #define ARCH_AARCH64
@@ -70,6 +72,12 @@
     #define ARCH_MIPS
 #elif defined(__sparc__) || defined(__sparc)
     #define ARCH_SPARC
+#elif defined(__riscv) && (__riscv_xlen == 64)
+    #define ARCH_RISCV
+    #define ARCH_RISCV64
+#elif defined(__riscv) && (__riscv_xlen == 32)
+    #define ARCH_RISCV
+    #define ARCH_RISCV32
 #endif
 
 //-----------------------------------------------------------------------------
@@ -179,28 +187,22 @@ namespace lsp
 
 //-----------------------------------------------------------------------------
 // Detect endianess and operations
-#if defined(ARCH_I386) || defined(ARCH_X86_64)
+#if defined(ARCH_X86) || defined(ARCH_X86_64)
     #define IF_ARCH_X86(...)        __VA_ARGS__
     #define ARCH_X86_ASM(...)       __asm__ __volatile__ ( __VA_ARGS__ )
+#endif /* ARCH_X86 */
 
-    #ifdef ARCH_I386
-        #define ARCH_I386_ASM(...)       __asm__ __volatile__ ( __VA_ARGS__ )
-    #endif /* ARCH_I386 */
+#if defined(ARCH_I386)
+    #define IF_ARCH_I386(...)       __VA_ARGS__
+    #define ARCH_I386_ASM(...)      __asm__ __volatile__ ( __VA_ARGS__ )
+    #define ARCH_STRING             "i386"
+#endif /* ARCH_I386 */
 
-    #ifdef ARCH_X86_64
-        #define ARCH_X86_64_ASM(...)       __asm__ __volatile__ ( __VA_ARGS__ )
-    #endif /* ARCH_I386 */
-
-    #define ARCH_X86
-
-    #if defined(ARCH_I386)
-        #define ARCH_STRING             "i386"
-        #define IF_ARCH_I386(...)       __VA_ARGS__
-    #else
-        #define ARCH_STRING             "x86_64"
-        #define IF_ARCH_X86_64(...)     __VA_ARGS__
-    #endif
-#endif /* defined(ARCH_I386) || defined(ARCH_X86_64) */
+#if defined(ARCH_X86_64)
+    #define IF_ARCH_X86_64(...)     __VA_ARGS__
+    #define ARCH_X86_64_ASM(...)    __asm__ __volatile__ ( __VA_ARGS__ )
+    #define ARCH_STRING             "x86_64"
+#endif /* defined(ARCH_X86_64) */
 
 #if defined(ARCH_ARM)
     #define IF_ARCH_ARM(...)            __VA_ARGS__
@@ -275,6 +277,25 @@ namespace lsp
 
     #define ARCH_STRING                 "SPARC"
 #endif /* defined(ARCH_PPC) */
+
+#if defined(ARCH_RISCV)
+    #define IF_ARCH_RISCV(...)          __VA_ARGS__
+    #define ARCH_RISCV_ASM(...)         __asm__ __volatile__ ( __VA_ARGS__ )
+#endif /* ARCH_RISCV */
+
+#if defined(ARCH_RISCV64)
+    #define IF_ARCH_RISCV64(...)        __VA_ARGS__
+    #define ARCH_RISCV64_ASM(...)       __asm__ __volatile__ ( __VA_ARGS__ )
+
+    #define ARCH_STRING                 "RISCV64"
+#endif /* defined(ARCH_RISCV64) */
+
+#if defined(ARCH_RISCV32)
+    #define IF_ARCH_RISCV32(...)        __VA_ARGS__
+    #define ARCH_RISCV32_ASM(...)       __asm__ __volatile__ ( __VA_ARGS__ )
+
+    #define ARCH_STRING                 "RISCV32"
+#endif /* defined(ARCH_RISCV32) */
 
 #if defined(ARCH_LE)
     #define __IF_LEBE(le, be)   le
@@ -614,6 +635,18 @@ namespace lsp
 #ifndef IF_ARCH_SPARC
     #define IF_ARCH_SPARC(...)
 #endif /* IF_ARCH_MIPS */
+
+#ifndef ARCH_RISCV_ASM
+    #define ARCH_RISCV_ASM(...)
+#endif /* ARCH_RISCV_ASM */
+
+#ifndef ARCH_RISCV64_ASM
+    #define ARCH_RISCV64_ASM(...)
+#endif /* ARCH_RISCV64_ASM */
+
+#ifndef ARCH_RISCV32_ASM
+    #define ARCH_RISCV32_ASM(...)
+#endif /* ARCH_RISCV32_ASM */
 
 //-----------------------------------------------------------------------------
 // Default platform
