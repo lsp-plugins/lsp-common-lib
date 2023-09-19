b476fc5095922d3d276e5da904d7d7b827d0da9a
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-09-19 22:24:47 +0300

    Added realloc_aligned function

--- include/lsp-plug.in/common/alloc.h
+++ include/lsp-plug.in/common/alloc.h
@@ -93,6 +93,42 @@ namespace lsp
             return reinterpret_cast<T *>((x & mask) ? ((x + align)&(~mask)) : x);
         }
 
+    /** Reallocate aligned pointer. Allocate new memory chunk if pointer was not previously allocated.
+     *
+     * @param ptr reference to pointer to store allocated pointer for future free() operation
+     * @param count number of elements to allocate
+     * @param align alignment, should be power of 2, by default DEFAULT_ALIGN
+     * @return aligned pointer as a result of alignment of ptr to align boundary or NULL if allocation failed
+     * @example
+     *      void *x = NULL;
+     *      float *a = realloc_aligned<float>(x, 1000); // Allocate 1000 floats aligned to DEFAULT_ALIGN boundary
+     *      if (a == NULL)
+     *          return ERROR;
+     *      // Do some stuff
+     *      free_aligned(x);
+     *      a = NULL;
+     */
+    template <class T, class P>
+        inline T *realloc_aligned(P * &ptr, size_t count, size_t align=DEFAULT_ALIGN)
+        {
+            // Check for power of 2
+            if ((!align) || (align & (align-1)))
+                return NULL;
+
+            // Allocate data
+            void *p         = ::realloc(ptr, (count * sizeof(T)) + align);
+            if (p == NULL)
+                return NULL;
+
+            // Store pointer
+            ptr             = reinterpret_cast<P *>(p);
+
+            // Return aligned pointer
+            ptrdiff_t x     = ptrdiff_t(p);
+            ptrdiff_t mask  = align-1;
+            return reinterpret_cast<T *>((x & mask) ? ((x + align)&(~mask)) : x);
+        }
+
     /** Free aligned pointer and write NULL to it
      *
      * @param ptr pointer to free
