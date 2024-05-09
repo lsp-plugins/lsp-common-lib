d27ca627e961018bafa4cc402b2dfcbb26133ced
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-05-09 13:07:25 +0300

    Added inplace new definition

---
+++ include/lsp-plug.in/common/new.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2024 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2024 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-common-lib
+ * Created on: 9 мая 2024 г.
+ *
+ * lsp-common-lib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-common-lib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-common-lib. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef LSP_PLUG_IN_COMMON_NEW_H_
+#define LSP_PLUG_IN_COMMON_NEW_H_
+
+#include <lsp-plug.in/common/version.h>
+#include <lsp-plug.in/common/types.h>
+
+#include <stddef.h>
+
+namespace lsp
+{
+    typedef struct inplace_new_tag_t
+    {
+    } inplace_new_tag_t;
+} /* namespace lsp */
+
+inline void *operator new(size_t size, void *ptr, const lsp::inplace_new_tag_t & tag)
+{
+    return ptr;
+}
+
+#endif /* INCLUDE_LSP_PLUG_IN_COMMON_NEW_H_ */
