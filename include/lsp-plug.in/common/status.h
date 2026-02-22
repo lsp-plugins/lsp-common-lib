2e4f3c1b82b4fc9cc3768adbef840cf6f3c3dd47
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-02-22 20:45:52 +0300

    Added new status codes

--- include/lsp-plug.in/common/status.h
+++ include/lsp-plug.in/common/status.h
@@ -41,72 +41,74 @@ namespace lsp
      */
     enum status_codes
     {
-        STATUS_OK,                    //!< STATUS_OK successful status
-        STATUS_UNSPECIFIED,           //!< STATUS_UNSPECIFIED
-        STATUS_LOADING,               //!< STATUS_LOADING
-        STATUS_IN_PROCESS,            //!< STATUS_IN_PROCESS
-        STATUS_UNKNOWN_ERR,           //!< STATUS_UNKNOWN_ERR
-        STATUS_NO_MEM,                //!< STATUS_NO_MEM
-        STATUS_NOT_FOUND,             //!< STATUS_NOT_FOUND
-        STATUS_BAD_FORMAT,            //!< STATUS_BAD_FORMAT
-        STATUS_UNSUPPORTED_FORMAT,    //!< STATUS_UNSUPPORTED_FORMAT
-        STATUS_CORRUPTED_FILE,        //!< STATUS_CORRUPTED_FILE
-        STATUS_NO_DATA,               //!< STATUS_NO_DATA
-        STATUS_INVALID_UID,           //!< STATUS_INVALID_UID
-        STATUS_DISCONNECTED,          //!< STATUS_DISCONNECTED
-        STATUS_BAD_ARGUMENTS,         //!< STATUS_BAD_ARGUMENTS
-        STATUS_NOT_BOUND,             //!< STATUS_NOT_BOUND
-        STATUS_BAD_STATE,             //!< STATUS_BAD_STATE
-        STATUS_NOT_IMPLEMENTED,       //!< STATUS_NOT_IMPLEMENTED
-        STATUS_ALREADY_EXISTS,        //!< STATUS_ALREADY_EXISTS
-        STATUS_OVERFLOW,              //!< STATUS_OVERFLOW
-        STATUS_BAD_HIERARCHY,         //!< STATUS_BAD_HIERARCHY
-        STATUS_DUPLICATED,            //!< STATUS_DUPLICATED
-        STATUS_TOO_BIG,               //!< STATUS_TOO_BIG
-        STATUS_PERMISSION_DENIED,     //!< STATUS_PERMISSION_DENIED
-        STATUS_IO_ERROR,              //!< STATUS_IO_ERROR
-        STATUS_NO_FILE,               //!< STATUS_NO_FILE
-        STATUS_EOF,                   //!< STATUS_EOF
-        STATUS_CLOSED,                //!< STATUS_CLOSED
-        STATUS_NOT_SUPPORTED,         //!< STATUS_NOT_SUPPORTED
-        STATUS_INVALID_VALUE,         //!< STATUS_INVALID_VALUE
-        STATUS_BAD_LOCALE,            //!< STATUS_BAD_LOCALE
-        STATUS_NO_DEVICE,             //!< STATUS_NO_DEVICE
-        STATUS_UNSUPPORTED_DEVICE,    //!< STATUS_UNSUPPORTED_DEVICE
-        STATUS_OPENED,                //!< STATUS_OPENED
-        STATUS_BAD_TYPE,              //!< STATUS_BAD_TYPE
-        STATUS_CORRUPTED,             //!< STATUS_CORRUPTED
-        STATUS_INSUFFICIENT,          //!< STATUS_INSUFFICIENT
-        STATUS_KILLED,                //!< STATUS_KILLED
-        STATUS_TIMED_OUT,             //!< STATUS_TIMED_OUT
-        STATUS_FAILED,                //!< STATUS_FAILED
-        STATUS_SKIP,                  //!< STATUS_SKIP
-        STATUS_CANCELLED,             //!< STATUS_CANCELLED
-        STATUS_NOT_EMPTY,             //!< STATUS_NOT_EMPTY
-        STATUS_IS_DIRECTORY,          //!< STATUS_IS_DIRECTORY
-        STATUS_NOT_DIRECTORY,         //!< STATUS_NOT_DIRECTORY
-        STATUS_REMOVED,               //!< STATUS_REMOVED
-        STATUS_BREAK_POINT,           //!< STATUS_BREAK_POINT  This is special status for step-by-step tracing algorithms
-        STATUS_READONLY,              //!< STATUS_READONLY
-        STATUS_NULL,                  //!< STATUS_NULL
-        STATUS_LOCKED,                //!< STATUS_LOCKED
-        STATUS_REJECTED,              //!< STATUS_REJECTED
-        STATUS_ALREADY_BOUND,         //!< STATUS_ALREADY_BOUND
-        STATUS_NO_CAPTURES,           //!< STATUS_NO_CAPTURES
-        STATUS_NO_SOURCES,            //!< STATUS_NO_SOURCES
-        STATUS_BAD_PATH,              //!< STATUS_BAD_PATH
-        STATUS_PROTOCOL_ERROR,        //!< STATUS_PROTOCOL_ERROR
-        STATUS_BAD_TOKEN,             //!< STATUS_BAD_TOKEN
-        STATUS_NO_GRAB,               //!< STATUS_NO_GRAB
-        STATUS_UNDERFLOW,             //!< STATUS_UNDERFLOW
-        STATUS_INCOMPATIBLE,          //!< STATUS_INCOMPATIBLE The module/function is not compatible
-        STATUS_NOT_MAPPED,            //!< STATUS_NOT_MAPPED the resource is not mapped
-        STATUS_RETRY,                 //!< STATUS_RETRY need to retry the call
-        STATUS_INTERRUPTED,           //!< STATUS_INTERRUPTED the call has been interrupted
+        STATUS_OK,                      //!< STATUS_OK successful status
+        STATUS_UNSPECIFIED,             //!< STATUS_UNSPECIFIED
+        STATUS_LOADING,                 //!< STATUS_LOADING
+        STATUS_IN_PROCESS,              //!< STATUS_IN_PROCESS
+        STATUS_UNKNOWN_ERR,             //!< STATUS_UNKNOWN_ERR
+        STATUS_NO_MEM,                  //!< STATUS_NO_MEM
+        STATUS_NOT_FOUND,               //!< STATUS_NOT_FOUND
+        STATUS_BAD_FORMAT,              //!< STATUS_BAD_FORMAT
+        STATUS_UNSUPPORTED_FORMAT,      //!< STATUS_UNSUPPORTED_FORMAT
+        STATUS_CORRUPTED_FILE,          //!< STATUS_CORRUPTED_FILE
+        STATUS_NO_DATA,                 //!< STATUS_NO_DATA
+        STATUS_INVALID_UID,             //!< STATUS_INVALID_UID
+        STATUS_DISCONNECTED,            //!< STATUS_DISCONNECTED
+        STATUS_BAD_ARGUMENTS,           //!< STATUS_BAD_ARGUMENTS
+        STATUS_NOT_BOUND,               //!< STATUS_NOT_BOUND
+        STATUS_BAD_STATE,               //!< STATUS_BAD_STATE
+        STATUS_NOT_IMPLEMENTED,         //!< STATUS_NOT_IMPLEMENTED
+        STATUS_ALREADY_EXISTS,          //!< STATUS_ALREADY_EXISTS
+        STATUS_OVERFLOW,                //!< STATUS_OVERFLOW
+        STATUS_BAD_HIERARCHY,           //!< STATUS_BAD_HIERARCHY
+        STATUS_DUPLICATED,              //!< STATUS_DUPLICATED
+        STATUS_TOO_BIG,                 //!< STATUS_TOO_BIG
+        STATUS_PERMISSION_DENIED,       //!< STATUS_PERMISSION_DENIED
+        STATUS_IO_ERROR  ,              //!< STATUS_IO_ERROR
+        STATUS_NO_FILE,                 //!< STATUS_NO_FILE
+        STATUS_EOF,                     //!< STATUS_EOF
+        STATUS_CLOSED,                  //!< STATUS_CLOSED
+        STATUS_NOT_SUPPORTED,           //!< STATUS_NOT_SUPPORTED
+        STATUS_INVALID_VALUE,           //!< STATUS_INVALID_VALUE
+        STATUS_BAD_LOCALE,              //!< STATUS_BAD_LOCALE
+        STATUS_NO_DEVICE,               //!< STATUS_NO_DEVICE
+        STATUS_UNSUPPORTED_DEVICE,      //!< STATUS_UNSUPPORTED_DEVICE
+        STATUS_OPENED,                  //!< STATUS_OPENED
+        STATUS_BAD_TYPE,                //!< STATUS_BAD_TYPE
+        STATUS_CORRUPTED,               //!< STATUS_CORRUPTED
+        STATUS_INSUFFICIENT,            //!< STATUS_INSUFFICIENT
+        STATUS_KILLED,                  //!< STATUS_KILLED
+        STATUS_TIMED_OUT,               //!< STATUS_TIMED_OUT
+        STATUS_FAILED,                  //!< STATUS_FAILED
+        STATUS_SKIP,                    //!< STATUS_SKIP
+        STATUS_CANCELLED,               //!< STATUS_CANCELLED
+        STATUS_NOT_EMPTY,               //!< STATUS_NOT_EMPTY
+        STATUS_IS_DIRECTORY,            //!< STATUS_IS_DIRECTORY
+        STATUS_NOT_DIRECTORY,           //!< STATUS_NOT_DIRECTORY
+        STATUS_REMOVED,                 //!< STATUS_REMOVED
+        STATUS_BREAK_POINT,             //!< STATUS_BREAK_POINT  This is special status for step-by-step tracing algorithms
+        STATUS_READONLY,                //!< STATUS_READONLY
+        STATUS_NULL,                    //!< STATUS_NULL
+        STATUS_LOCKED,                  //!< STATUS_LOCKED
+        STATUS_REJECTED  ,              //!< STATUS_REJECTED
+        STATUS_ALREADY_BOUND,           //!< STATUS_ALREADY_BOUND
+        STATUS_NO_CAPTURES,             //!< STATUS_NO_CAPTURES
+        STATUS_NO_SOURCES,              //!< STATUS_NO_SOURCES
+        STATUS_BAD_PATH,                //!< STATUS_BAD_PATH
+        STATUS_PROTOCOL_ERROR,          //!< STATUS_PROTOCOL_ERROR
+        STATUS_BAD_TOKEN,               //!< STATUS_BAD_TOKEN
+        STATUS_NO_GRAB,                 //!< STATUS_NO_GRAB
+        STATUS_UNDERFLOW,               //!< STATUS_UNDERFLOW
+        STATUS_INCOMPATIBLE,            //!< STATUS_INCOMPATIBLE The module/function is not compatible
+        STATUS_NOT_MAPPED,              //!< STATUS_NOT_MAPPED the resource is not mapped
+        STATUS_RETRY,                   //!< STATUS_RETRY need to retry the call
+        STATUS_INTERRUPTED,             //!< STATUS_INTERRUPTED the call has been interrupted
+        STATUS_NOT_FILE,                //!< STATUS_NOT_FILE not a regular file
+        STATUS_BAD_SYMLINK,             //!< STATUS_BAD_SYMMLINK bad symbolic link
 
         STATUS_TOTAL,
         STATUS_MAX = STATUS_TOTAL - 1,
-        STATUS_SUCCESS = STATUS_OK    //!< STATUS_SUCCESS an alias to STATUS_OK
+        STATUS_SUCCESS = STATUS_OK      //!< STATUS_SUCCESS an alias to STATUS_OK
     };
 
     /**
